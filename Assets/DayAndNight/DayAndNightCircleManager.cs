9f82289d7a5ce3120062b8149d6bab81f3919a5a
tolgakrc <tolgakaraca.official@gmail.com>
2023-07-18 21:56:55 +0300

    Day and Night Ciycle added.

---
+++ Assets/DayAndNight/DayAndNightCircleManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+namespace DayAndNight
+{
+    public class DayAndNightCircleManager : MonoBehaviour
+    {
+        [SerializeField]private Vector3 startPosSun;
+        [SerializeField]private Vector3 startPosMoon;
+        [SerializeField]private float delay;
+        [SerializeField]private float steps;
+        [SerializeField] private Light sun;
+        [SerializeField] private Light moon;
+        [SerializeField] private Color skyColorInSunAtTop;
+        [SerializeField] private Color skyColorInSunAtSide;
+        [SerializeField] private Color skyColorInMoonAtTop;
+        [SerializeField] private Color skyColorInMoonAtSide;
+        
+        private Material _shaderMat;
+        private float _timeLeft;
+        private static readonly int ZenitColorM = Shader.PropertyToID("_ZenitColorM");
+        private const float EulerAngleForAnHour = 15;
+
+        private void Awake()
+        {
+            _shaderMat = RenderSettings.skybox;
+
+        }
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.forward * EulerAngleForAnHour/steps * Time.deltaTime);
+
+            var lastAngle = transform.localEulerAngles.z;
+            
+            if (lastAngle%360<45)
+            {
+                SetSkyColor(skyColorInSunAtSide);
+                moon.enabled = false;
+                sun.enabled = true;
+                sun.intensity = 1;
+                sun.shadowStrength = .002f;
+            }
+            else if (lastAngle%360>=45 &&lastAngle%360<135 )
+            {
+                SetSkyColor(skyColorInSunAtSide);
+                SetLight(sun,1.5f,.1f,true);
+                
+            }
+            else if (lastAngle%360>=135 &&lastAngle%360<180)
+            {
+                SetSkyColor(skyColorInSunAtTop);
+                SetLight(sun,1f,.5f,false);
+            }
+            else if (lastAngle%360>=180 &&lastAngle%360<225)
+            {
+                SetSkyColor(skyColorInMoonAtSide);
+                _shaderMat.SetColor(ZenitColorM,skyColorInMoonAtSide);
+                moon.enabled = true;
+                sun.enabled = false;
+                moon.intensity = .7f;
+                moon.shadowStrength = .1f;
+            }
+            else if (lastAngle%360>=225 &&lastAngle%360<315)
+            {
+                SetSkyColor(skyColorInMoonAtTop);
+                SetLight(moon,1,.3f,true);
+            }
+            else
+            {
+                SetSkyColor(skyColorInMoonAtSide);
+                SetLight(moon,.7f,.1f,false);
+            }
+        }
+
+        private void SetLight(Light selectedLight,float intensityBorder,float shadowStrengthBorder,bool isShining)
+        {
+            if (isShining)
+            {
+                if (selectedLight.intensity < intensityBorder)
+                {
+                    selectedLight.intensity += .002f;
+                }
+
+                if (selectedLight.shadowStrength < shadowStrengthBorder)
+                {
+                    selectedLight.shadowStrength += .002f;
+                }
+            }
+            else
+            {
+                if (selectedLight.intensity>intensityBorder)
+                {
+                    selectedLight.intensity -= .002f;
+                }
+
+                if (selectedLight.shadowStrength > shadowStrengthBorder)
+                {
+                    moon.shadowStrength -= .002f;
+                }
+            }
+        }
+
+        private void SetSkyColor(Color selectedColor)
+        {
+            if (_shaderMat.GetColor(ZenitColorM) != selectedColor)
+            {
+                var lerpedColor = Color.Lerp(_shaderMat.GetColor(ZenitColorM), selectedColor, .1f);
+                _shaderMat.SetColor(ZenitColorM, lerpedColor);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/PseudoDictionary.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extensions
+{
+    [System.Serializable]
+    public class PseudoDictionary<T, TU>
+    {
+    // PSEUDO DICTIONARY ENTRIES
+    // & DICTIONARY CONVERSION
+ 
+    [SerializeField] private List<PseudoKeyValuePair<T, TU>> entries;
+    private Dictionary<T, TU> _actualDictionary = new();
+ 
+    // COUNT
+ 
+    public int Count
+    {
+        get
+        {
+            _actualDictionary = FromPseudoDictionaryToActualDictionary();
+            return _actualDictionary.Count;
+        }
+    }
+ 
+    // INDEXER
+ 
+    public TU this[T index]
+    {
+        get
+        {
+            _actualDictionary = FromPseudoDictionaryToActualDictionary();
+            return _actualDictionary[index];
+        }
+    }
+ 
+    // FROM DICTIONARY TO PSEUDO
+ 
+    public List<PseudoKeyValuePair<T, TU>> FromActualDictionaryToPseudoDictionary(Dictionary<T, TU> actualDictionary)
+    {
+        List<PseudoKeyValuePair<T, TU>> pseudoDictionary = new();
+ 
+        foreach (KeyValuePair<T, TU> pair in actualDictionary)
+            pseudoDictionary.Add(new(pair.Key, pair.Value));
+ 
+        return pseudoDictionary;
+    }
+ 
+    public List<PseudoKeyValuePair<T, TU>> FromActualDictionaryToPseudoDictionary()
+        => FromActualDictionaryToPseudoDictionary(_actualDictionary);
+ 
+    // FROM PSEUDO TO DICTIONARY
+ 
+    public Dictionary<T, TU> FromPseudoDictionaryToActualDictionary(List<PseudoKeyValuePair<T, TU>> pseudoDictionary)
+    {
+        Dictionary<T, TU> dictionary = new();
+ 
+        foreach (PseudoKeyValuePair<T, TU> entry in pseudoDictionary)
+            dictionary.Add(entry.Key, entry.Value);
+ 
+        return dictionary;
+    }
+ 
+    public Dictionary<T, TU> FromPseudoDictionaryToActualDictionary()
+        => FromPseudoDictionaryToActualDictionary(entries);
+ 
+    // OPERATIONS
+ 
+    public void Add(T key, TU value)
+    {
+        _actualDictionary = FromPseudoDictionaryToActualDictionary();
+        _actualDictionary.Add(key, value);
+        entries = FromActualDictionaryToPseudoDictionary();
+    }
+ 
+    public void Remove(T key)
+    {
+        _actualDictionary = FromPseudoDictionaryToActualDictionary();
+        _actualDictionary.Remove(key);
+        entries = FromActualDictionaryToPseudoDictionary();
+    }
+ 
+    public void Clear()
+    {
+        _actualDictionary.Clear();
+        entries = new();
+    }
+ 
+    public TU TryGetValue(T key)
+    {
+        _actualDictionary = FromPseudoDictionaryToActualDictionary();
+        _actualDictionary.TryGetValue(key, out var value);
+        return value;
+    }
+}
+
+    [System.Serializable]
+    public struct PseudoKeyValuePair<T, TU>
+    {
+        [SerializeField] private T key;
+        [SerializeField] private TU value;
+
+        public T Key => key;
+
+        public TU Value => value;
+
+        public PseudoKeyValuePair(T key, TU value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
